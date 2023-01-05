3c2b16c4747ccf38e7cc7ac0833e222b4305eda8
ughazi <ghaziumair@hotmail.com>
2023-01-05 14:02:09 -0500

    Update customRender function to include routes

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { UserDataContext } from '../contexts/UserData.context';
 
-const customRender = (ui, { providerProps, ...renderOptions }) => {
+const customRender = (ui, { providerProps, route = '/', ...renderOptions }) => {
+  window.history.pushState({}, 'Test page', route);
   return render(
     <UserDataContext.Provider value={{ ...providerProps }}>
       <BrowserRouter>{ui}</BrowserRouter>
